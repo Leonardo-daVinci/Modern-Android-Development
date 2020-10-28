5b6427def899da9db7f7133083ae8f078c8ee3cb
Akshit Keoliya <akshitkeoliya4@gmail.com>
2020-10-28 14:20:32 +0530

    ImageSearchApp - Layouts and Model class

---
+++ ImageSearchApp/app/src/main/java/apps/nocturnuslabs/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,29 @@
+package apps.nocturnuslabs.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+//Parcelable is used to transfer this data between fragments using Navigation component
+//the only types that are allowed are int/string/parcelable
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String,
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
+    }
+}
